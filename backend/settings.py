cfa6596c372557110f62047ecde9cee15b0a8119
Saurabh9312 <saurabhparate054@gmail.com>
2026-03-01 09:07:56 +0530

    database changed

--- backend/settings.py
+++ backend/settings.py
@@ -86,14 +86,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'daily_budget_buddy',
+        'USER': 'daily_budget_buddy_saurabh',
+        'PASSWORD': 'Sau_rabh@123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
-db_from_env = dj_database_url.config(conn_max_age=600)
-if db_from_env:
-    DATABASES['default'].update(db_from_env)
 
 
 # Password validation
